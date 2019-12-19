9951b707c427c4d0c9ab2b85e95f65a6beeed302
Jenny Kam <jkam17@gmail.com>
2019-12-19 16:45:06 -0500

    define initial RootQuery for specific BookType

--- schema/schema.js
+++ schema/schema.js
@@ -1,12 +1,32 @@
 const graphql = require('graphql');
 
-const {GraphQLObjectType, GraphQLString} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+//destructuring, grabs GraphQLString... from graphql package. must be these var names.
 
-const BookType = new GraphQLObjectType({
+const BookType = new GraphQLObjectType({    //define new object type
   name: 'Book',
-  fields: () => ({
-    id: { type: GraphQLString },
+  fields: () => ({   //wrap in fn so objects can interact/reference each other better
+    id: { type: GraphQLString },    //must use GraphQLString, not string
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
+})
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields:{
+    book:{
+      type: BookType,
+      args:{ id:{ type: GraphQLString }},  //expected arg when querying, ex query: book(id:"123"){name} ->retrieve name of book 123
+      resolve(parent, args){ //parent refers to a relationship, args refers to prev line
+        //fires when queried, will have access to args.id in this case
+        //code to get data from db or other source
+      }
+    }
+  }
+})
+
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
