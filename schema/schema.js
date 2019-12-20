be2a2bfc87dc894eb0279254b05ac52b20458942
Jenny Kam <jkam17@gmail.com>
2019-12-20 15:07:10 -0500

    add dummy book data to schema and a basic resolve fn for querying a book

--- schema/schema.js
+++ schema/schema.js
@@ -3,6 +3,13 @@ const graphql = require('graphql');
 const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
 //destructuring, grabs GraphQLString... from graphql package. must be these var names.
 
+//dummy data
+const books = [
+  {name: 'A Dance With Dragons', genre: 'Fantasy', id: '1'},
+  {name: 'Watchmen', genre: 'Fantasy', id: '2'},
+  {name: 'GDC', genre: 'Xianxia', id: '3'}
+]
+
 const BookType = new GraphQLObjectType({    //define new object type
   name: 'Book',
   fields: () => ({   //wrap in fn so objects can interact/reference each other better
@@ -12,7 +19,7 @@ const BookType = new GraphQLObjectType({    //define new object type
   })
 })
 
-const RootQuery = new GraphQLObjectType({
+const RootQuery = new GraphQLObjectType({   //used to jump into graph to query
   name: "RootQueryType",
   fields:{
     book:{
@@ -20,7 +27,8 @@ const RootQuery = new GraphQLObjectType({
       args:{ id:{ type: GraphQLString }},  //expected arg when querying, ex query: book(id:"123"){name} ->retrieve name of book 123
       resolve(parent, args){ //parent refers to a relationship, args refers to prev line
         //fires when queried, will have access to args.id in this case
-        //code to get data from db or other source
+        //code to get data from db/other source
+        return books.find( book => book.id === args.id )
       }
     }
   }
