886924250d45e8d793d8458799bf75c22aebfb03
Jenny Kam <jkam17@gmail.com>
2020-01-14 21:35:18 -0500

    set up mongoose mongodb connection in app.js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express")
 const graphqlHTTP = require("express-graphql")
 const schema = require("./schema/schema")
+const mongoose = require ("mongoose")
 
 const app = express()
+
+//connect to mongodb database: mongo-jk,ukghJCiUymue01e1
+mongoose.connect('mongodb+srv://mongo-jk:ukghJCiUymue01e1@node-gql-pract-vdd94.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true , useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err => console.log("Error: ", err.message));
+// mongoose.connection.once('open', () => {
+//   console.log('connected to database')
+// })
 
 app.get('/', function (req, res) {
   res.send('Hello Worrrrld!!!!!!!!!!');
