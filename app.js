790be42ed0e224fb4d3a05cebb780923942799e7
Jenny Kam <jkam17@gmail.com>
2020-01-15 21:23:49 -0500

    add mongodb model for books

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const app = express()
 mongoose.connect('mongodb+srv://mongo-jk:ukghJCiUymue01e1@node-gql-pract-vdd94.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true , useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB Atlas"))
   .catch(err => console.log("Error: ", err.message));
+
+//replaced by then/catch above
 // mongoose.connection.once('open', () => {
 //   console.log('connected to database')
 // })
