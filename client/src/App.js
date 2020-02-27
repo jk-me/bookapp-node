70fca6aa32ab4478b97b3123cab044f50c3f8f55
Jenny Kam <jkam17@gmail.com>
2020-02-27 18:40:58 -0500

    add comments

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import ApolloProvider from '@apollo/react-hooks'
 
 import BookList from './BookList.js'
 
+//apollo client setup
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql"
 })
 
 function App() {
   return (
+    //wrap w ApolloProvider and add set up ApolloClient
     <ApolloProvider client={client}>
       <div className="App">
         <BookList />
